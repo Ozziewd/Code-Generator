94857a56d5836ffa27b68784bfc5a87a96f58b13
Osvaldo Ruiz <owebstudent@gmail.com>
2019-11-03 23:55:40 -0500

    Created one variable with all the characters and will use math.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var pNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var PUpper = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var pLower = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var pSpecial = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",]
+var everyCha = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"]
 document.getElementById("button1").maxLength = "128"
 document.getElementById("button1").minLength = "8"
-for (i = 0; i < pLower.length; i++){
-    
+for (i = 0; i < pLower.length; i++) {
+
+}
+function charGen() {
+    for (i = 0; i < everyCha.length; i++) {
+
+    }
 }
 Math.floor.Math.Random
