09c76889381698c80c2243d84896ac22c31075d2
Osvaldo Ruiz <owebstudent@gmail.com>
2019-11-03 23:05:02 -0500

    Added a few codes I know Ill need in order for the generator to work

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-var pNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9","0"]
-var PUpper = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var pLower = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var pSpecial = ["!","#","$","%","&","*","+","-",".","/",":",";","<","=",">","?","@",]
+var pNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var PUpper = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var pLower = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var pSpecial = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",]
+document.getElementById("button1").maxLength = "128"
+document.getElementById("button1").minLength = "8"
+for (i = 0; i < pLower.length; i++){
+    
+}
+Math.floor.Math.Random
